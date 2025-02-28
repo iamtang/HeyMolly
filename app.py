46855fc1c0e966c452214465d5f003e14304949d
jj <tangjun@zuzuche.com>
2025-02-28 16:07:59 +0800

    add

--- app.py
+++ app.py
@@ -1,120 +1,129 @@
-import asyncio
-import struct
-import pvporcupine
-import pyaudio
+from snowboy import snowboydecoder
 import speech_recognition as sr
-import re
-from dotenv import load_dotenv
-import os
+# import asyncio
 from AI import AI
-from TTS import TTS
+from BAIDU_API import API
 from MusicPlayer import MusicPlayer
+# import re
+import time
+import os
+from dotenv import load_dotenv
 
+MODEL_FILE = "assets/heymolly.pmdl"  # 替换为你的模型文件
 load_dotenv()
-base_url = os.getenv("BASE_URL")
-api_key = os.getenv("API_KEY")
-model = os.getenv("MODEL")
-
-appid = os.getenv("APPID")
-token = os.getenv("TOKEN")
-cluster = os.getenv("CLUSTER")
-voice_type = os.getenv("VOICE_TYPE")
 
 ai = AI(
-    base_url=base_url,
-    api_key=api_key,
-    model=model # ep-20250219163101-p5zk6
+    base_url=os.getenv("BASE_URL"),
+    api_key=os.getenv("API_KEY"),
+    model=os.getenv("MODEL")
 )
-
-tts = TTS(
-    appid=appid,
-    token=token,
-    cluster=cluster,
-    voice_type=voice_type,
+api = API(
+    api_key=os.getenv("BAIDU_API_KEY"),
+    secret_key=os.getenv("BAIDU_SECRET_KEY")
 )
+# tts = TTS(
+#     appid=os.getenv("APPID"),
+#     token=os.getenv("TOKEN"),
+#     cluster=os.getenv("CLUSTER"),
+#     voice_type=os.getenv("VOICE_TYPE"),
+# )
+# text = ""
+# frist=False
 
-text = ""
-frist=False
-# 初始化语音识别
-recognizer = sr.Recognizer()
 mic = sr.Microphone()
-# 初始化 Porcupine 唤醒词检测
-porcupine = pvporcupine.create(
-    access_key="XcEt9naMKyhAYoKbNr6eJyRI8wqSX0VWkQ0/r7xGp6QAP42BMWqi0Q==",
-    keyword_paths=["./hey-molly_en_mac_v3_0_0.ppn"]
-)
-
-# 初始化麦克风音频流
-audio = pyaudio.PyAudio()
-voice_stream = audio.open(
-    input_device_index=0,
-    rate=porcupine.sample_rate,
-    channels=1,
-    format=pyaudio.paInt16,
-    input=True,
-    frames_per_buffer=porcupine.frame_length
-)
-async def callback(file, t, isEnd):
-    global text, frist
-    text += t
-    if('。' in text):
-        match = re.search(r'([^。]+[。])(.*)', text)
-        if(match != None):
-            await tts.run(match.group(1), file)
-            frist = True
-            text = match.group(2)
-    elif isEnd:
-        await tts.run(text, file)
-        frist = True
-
-async def listen_and_transcribe():
+recognizer = sr.Recognizer()
+recognizer.pause_threshold = 1.0
+def rmFile(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    else:
+        print(f"{file_path} 不存在")
+# 回调函数，当检测到唤醒词时触发
+def listen_and_transcribe():
+    ai.delete_chat_history()
     """监听用户语音并转成文本"""
+    player = MusicPlayer('assets/zai.mp3')
+    player.play()
     with mic as source:
-        recognizer.adjust_for_ambient_noise(source)  # 调整噪音水平
-        print("请开始说话...")
-        try:
-            audio_data = recognizer.listen(source, timeout=3,)  # 监听语音
-            text = recognizer.snowboy_wait_for_hot_word(audio_data, language="zh-CN")  # 语音转文本
-            print(f"用户说: {text}")
-            await say(text)
-        except sr.WaitTimeoutError:
-            print("未检测到语音输入")
-        except sr.UnknownValueError:
-            print("无法识别语音")
-        except sr.RequestError:
-            print("语音识别请求失败")
+        while 1:
+            path = 'audio/chat.pcm'
+            recognizer.adjust_for_ambient_noise(source, duration=1)  # 调整噪音水平
+            recognizer.energy_threshold *= 1.5
+            print("请说话...")
+            try:
+                start_time = time.time()
+                audio_data = recognizer.listen(source, timeout=3)  # 监听语音
+                end_time = time.time()
+                print(end_time - start_time)
+                wav_data = audio_data.get_wav_data()  # 获取 WAV 格式的二进制数据
+                
+                # 去除 WAV 头部，只保留 PCM 数据
+                pcm_data = wav_data[44:]  # 前 44 字节是 WAV 头部，去掉它
+                # 将 PCM 数据保存到文件
+                with open(path, "wb") as f:
+                    f.write(pcm_data)
 
-async def say(text):
-    global frist
-    frist=False
-    await tts.connect()
-    asyncio.create_task(ai.send(text, callback))
-    while frist == False:
-        await asyncio.sleep(1)
-    player = MusicPlayer('chat.mp3')
+                print("音频已保存为 " + path)
+                text = api.asr(path)
+                # rmFile(path)
+                print(f"用户说: {text}")
+                if text == '':
+                    break
+                break
+                ask(text)
+            except sr.WaitTimeoutError:
+                print("未检测到语音输入")
+                break
+            except sr.UnknownValueError:
+                print("无法识别语音")
+                break
+            except sr.RequestError:
+                print("语音识别请求失败")
+                break
+
+def ask(text):
+    # global frist
+    # frist=False
+    filename = 'audio/reply.mp3'
+    resText = ai.send(text)
+    print(f"AI说: {resText}")
+    api.tts(resText, filename)
+    # while frist == False:
+    #     await asyncio.sleep(1)
+    player = MusicPlayer(filename)
     player.play()
     while True:
-        await asyncio.sleep(1)
+        time.sleep(1)
         if player.is_finished():
             print('====finished==')
             break
+    # 清空记录
+    rmFile(filename)
 
-async def main():
-    while True:
-        print("正在监听唤醒词...")
-        audio_data = voice_stream.read(porcupine.frame_length, exception_on_overflow=False)
-        audio_data_unpacked = struct.unpack_from("h" * porcupine.frame_length, audio_data)
-        
-        # 处理唤醒词
-        keyword_index = porcupine.process(audio_data_unpacked)
-        if keyword_index >= 0:
-            player = MusicPlayer('zai.mp3')
-            player.play()
-            await listen_and_transcribe()  # 监听用户语音
+
+# def callback(t, file, isEnd):
+#     global text, frist
+#     text += t or ""
+#     if('。' in text):
+#         match = re.search(r'([^。]+[。])(.*)', text)
+#         if(match != None):
+#             tts.run(match.group(1), file)
+#             frist = True
+#             text = match.group(2)
+#     elif isEnd:
+#         tts.run(text, file)
+#         frist = True
+
+def main():
+    # 初始化 Snowboy 检测器
+    detector = snowboydecoder.HotwordDetector(MODEL_FILE, sensitivity=0.5)
+    print("开始监听唤醒词...")
+    # 开始监听
+    detector.start(listen_and_transcribe, sleep_time=0.03)  # 设置检测间隔
+    # 停止监听
+    detector.terminate()
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
-    # 停止唤醒词检测
-    # porcupine.delete()
-    # voice_stream.close()
-    # audio.terminate()
+    # api.asr("output.pcm")
+    listen_and_transcribe()
--- tt.py
+++
@@ -1,5 +0,0 @@
-from TTS_BAIDU import TTS
-
-
-tts = TTS()
-tts.run('在 Python 中，要实现持久化存储并支持过期时间，可以使用一些常见的库和方法，例如使用 pickle 和 shelve 库，或者通过使用 Redis 等数据库来实现缓存存储。', 'chat.mp3')
